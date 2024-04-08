bb46a31985c19b401014a4440d4430f16044345e
LeoKLis <leoklisanic@gmail.com>
2024-04-08 23:41:41 +0200

    Hamburger meni i pozdrav dodan
    Napravljen hamburger meni gdje se klikom na ikonu prikaze cijeli meni.
    Napravljen pozdrav 'Dobrodosli' i smanjena neprozirnost glavne slike.

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+function show_nav(){
+    var x = document.getElementById("nav_links");
+    if(x.style.display === "block"){
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
