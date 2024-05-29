bbc0c2c49b12d637c29c79fb188b0affb237f4a3
LeoKLis <leoklisanic121@gmail.com>
2024-05-29 17:16:21 +0200

    Dodan express i node server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path')
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+const indexRouter = require('./routes/index.routes');
+const cartRouter = require('./routes/cart.routes');
+
+app.use('/home', indexRouter);
+app.use('/cart', cartRouter);
+
+app.listen(3000);
